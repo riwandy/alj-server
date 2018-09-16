735e36d19889302b4f8e77629af0c26063fc2733
riwandy <riwandy@outlook.com>
2018-09-16 23:58:30 +0800

    fixed

--- src/models/index.js
+++ src/models/index.js
@@ -23,11 +23,11 @@ fs
     })  
 
 // db.Employee.hasMany(db.Timesheet)
-// db.Timesheet.belongsTo(db.Employee, {foreignKey : 'employeeID', targetKey : 'employeeID'})
-// db.Timesheet.belongsTo(db.Project, {foreignKey : 'projectID', targetKey : 'projectID'})
-// db.User.belongsTo(db.Employee, {foreignKey : 'employeeID', targetKey : 'employeeID'})
-// db.Project.belongsTo(db.Employee, {foreignKey : 'supervisor', targetKey : 'employeeID'})
-// db.Project.belongsTo(db.Client, {foreignKey : 'clientID', targetKey : 'clientID'})
+db.Timesheet.belongsTo(db.Employee, {foreignKey : 'employeeID', targetKey : 'employeeID'})
+db.Timesheet.belongsTo(db.Project, {foreignKey : 'projectID', targetKey : 'projectID'})
+db.User.belongsTo(db.Employee, {foreignKey : 'employeeID', targetKey : 'employeeID'})
+db.Project.belongsTo(db.Employee, {foreignKey : 'supervisor', targetKey : 'employeeID'})
+db.Project.belongsTo(db.Client, {foreignKey : 'clientID', targetKey : 'clientID'})
 // db.Employee.hasMany(db.Timesheet, {foreignKey : 'employeeID', targetKey : 'employeeID'})
 
 
